1197a2c1e52d85ae16698d2db61a1ca34568d74f
심민성 <75308777+owl1753@users.noreply.github.com>
2024-11-15 15:19:24 +0900

    feat:  header 및 footer 추가 (#1)
    * feat: Header 및 Footer 추가
    
    * design: Header 디자인 수정

---
+++ src/routes/-components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import {StyledHeader, StyledLink, StyledNav, StyledSpace} from "./Header.style.ts";
+
+function Header() {
+    return <StyledHeader>
+        <StyledNav>
+            <StyledLink to="/">
+                공간오픈/관리
+            </StyledLink>
+            <StyledLink to="/about">
+                공간찾기
+            </StyledLink>
+            <StyledLink>
+                교내회의실
+            </StyledLink>
+            <StyledLink>
+                로그인
+            </StyledLink>
+            <StyledLink>
+                로그아웃
+            </StyledLink>
+        </StyledNav>
+        <StyledSpace />
+    </StyledHeader>
+}
+
+export default Header;
