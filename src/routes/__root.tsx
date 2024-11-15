db256fccd8cde2b466f3eb76d04e1dc9b5b16da6
심민성 <75308777+owl1753@users.noreply.github.com>
2024-11-15 15:19:24 +0900

    feat:  header 및 footer 추가 (#1)
    * feat: Header 및 Footer 추가
    
    * design: Header 디자인 수정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,20 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import styled from "styled-components";
+import Header from "./-components/Header/Header.tsx";
+import Footer from "./-components/Footer/Footer.tsx";
+
+const StyledRoot = styled.div`
+    min-height: 100vh;
+`
 
 export const Route = createRootRoute({
     component: () => (
-        <>
-            <div className="p-2 flex gap-2">
-                <Link to="/" className="[&.active]:font-bold">
-                    Home
-                </Link>{' '}
-                <Link to="/about" className="[&.active]:font-bold">
-                    About
-                </Link>
-            </div>
-            <hr />
+        <StyledRoot>
+            <Header />
             <Outlet />
+            <Footer />
             <TanStackRouterDevtools />
-        </>
+        </StyledRoot>
     ),
 })
