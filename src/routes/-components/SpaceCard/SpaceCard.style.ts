6e0cf144f482eea736742fb674bf4686511eee64
solmee <kii5541@naver.com>
2024-11-18 19:41:09 +0900

    feat: 개설된 공간 정보 SpaceCard 컴포넌트 구현

---
+++ src/routes/-components/SpaceCard/SpaceCard.style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+  border-radius: 12px;
+  overflow: hidden;
+  border: 1px solid #edf0f3;
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: 160px;
+  object-fit: cover;
+`;
+
+export const StyledContent = styled.div`
+  padding: 20px 60px;
+  background: white;
+`;
+
+export const StyledTitledRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 8px;
+  padding: 20px 60px;
+`;
+
+export const StyledRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 8px;
+`;
+
+export const StyledName = styled.div`
+  font-size: 22px;
+  font-weight: 500;
+`;
+
+export const StyledLocation = styled.div`
+  font-size: 22px;
+  color: #666;
+`;
+
+export const StyledTime = styled.div`
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 8px;
+`;
+
+export const StyledCapacity = styled.div`
+  font-size: 16px;
+  color: #666;
+`;
+
+export const StyledTitle = styled.h2`
+  font-size: 22px;
+  font-weight: 500;
+  color: #333;
+`;
