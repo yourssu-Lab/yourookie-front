ecf17867fd4165c1548e96f366da13e770d42b39
solmee <kii5541@naver.com>
2024-11-19 20:32:44 +0900

    fix: SpaceCard 스타일 수정

--- src/routes/-components/SpaceCard/SpaceCard.style.ts
+++ src/routes/-components/SpaceCard/SpaceCard.style.ts
@@ -1,58 +1,69 @@
 import styled from "styled-components";
 
 export const StyledCard = styled.div`
+  background: white;
   border-radius: 12px;
   overflow: hidden;
   border: 1px solid #edf0f3;
 `;
 
-export const StyledImage = styled.img`
+export const StyledSpaceImage = styled.div`
   width: 100%;
-  height: 160px;
-  object-fit: cover;
+  height: 150px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
 
-export const StyledContent = styled.div`
+export const StyledSpaceContent = styled.div`
   padding: 20px 60px;
-  background: white;
 `;
 
-export const StyledTitledRow = styled.div`
+export const StyledHeader = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 8px;
   padding: 20px 60px;
 `;
 
-export const StyledRow = styled.div`
+export const StyledSpaceHeader = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 8px;
 `;
 
-export const StyledName = styled.div`
-  font-size: 22px;
-  font-weight: 500;
+export const StyledSpaceLocation = styled.div`
+  font-size: 20px;
+  font-weight: 600;
 `;
 
-export const StyledLocation = styled.div`
-  font-size: 22px;
+export const StyledSpaceInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   color: #666;
+  font-size: 14px;
 `;
 
-export const StyledTime = styled.div`
-  font-size: 16px;
-  color: #666;
-  margin-bottom: 8px;
-`;
+export const StyledSpaceTime = styled.div``;
 
-export const StyledCapacity = styled.div`
-  font-size: 16px;
-  color: #666;
-`;
+export const StyledSpaceCapacity = styled.div``;
 
 export const StyledTitle = styled.h2`
   font-size: 22px;
   font-weight: 500;
   color: #333;
+`;
+
+export const StyledEditIcon = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  img {
+    width: 24px;
+    height: 24px;
+  }
 `;
