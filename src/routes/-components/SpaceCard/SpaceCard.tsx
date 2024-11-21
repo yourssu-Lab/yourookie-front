08097fdf872a808928b444a47ecd860d74af2d05
solmee <87332713+ssolfa@users.noreply.github.com>
2024-11-22 04:36:18 +0900

    fix: 1-1 디자인 반영 (#9)
    * asset: svg 파일 추가
    
    * mock: mock 데이터 수정
    
    * fix: 디자인 반영

--- src/routes/-components/SpaceCard/SpaceCard.tsx
+++ src/routes/-components/SpaceCard/SpaceCard.tsx
@@ -1,16 +1,20 @@
 import pencil from "../../../assets/Pencil.svg";
+import clock from "../../../assets/orga/clock.svg";
+import marker from "../../../assets/orga/marker.svg";
+import user from "../../../assets/orga/user.svg";
+
 import { Space } from "../../../types/organization.type";
 import {
+  StyledCapacityInfo,
   StyledCard,
   StyledEditIcon,
-  StyledHeader,
-  StyledSpaceCapacity,
+  StyledLeftContent,
+  StyledRightContent,
   StyledSpaceContent,
-  StyledSpaceHeader,
   StyledSpaceImage,
   StyledSpaceInfo,
   StyledSpaceLocation,
-  StyledSpaceTime,
+  StyledTimeInfo,
   StyledTitle,
 } from "./SpaceCard.style";
 
@@ -22,25 +26,31 @@ interface SpaceCardProps {
 export const SpaceCard = ({ space, onEditClick }: SpaceCardProps) => {
   return (
     <StyledCard>
-      <StyledHeader>
-        <StyledTitle>{space.name}</StyledTitle>
+      <StyledSpaceImage>
+        <img src={space.spaceImageUrl} alt={space.name} />
         <StyledEditIcon onClick={onEditClick}>
           <img src={pencil} alt="수정하기" />
         </StyledEditIcon>
-      </StyledHeader>
-      <StyledSpaceImage>
-        <img src={space.spaceImageUrl} alt={space.name} />
       </StyledSpaceImage>
-
       <StyledSpaceContent>
-        <StyledSpaceHeader>
-          <StyledSpaceLocation>{space.location}</StyledSpaceLocation>
-        </StyledSpaceHeader>
         <StyledSpaceInfo>
-          <StyledSpaceTime>
-            {space.openingTime} ~ {space.closingTime}
-          </StyledSpaceTime>
-          <StyledSpaceCapacity>최대 {space.capacity}인</StyledSpaceCapacity>
+          <StyledLeftContent>
+            <StyledTitle>{space.name}</StyledTitle>
+            <StyledSpaceLocation>
+              <img src={marker} alt="장소 위치" />
+              {space.location}
+            </StyledSpaceLocation>
+          </StyledLeftContent>
+          <StyledRightContent>
+            <StyledTimeInfo>
+              <img src={clock} alt="운영 시간" />
+              {space.openingTime}~{space.closingTime}
+            </StyledTimeInfo>
+            <StyledCapacityInfo>
+              <img src={user} alt="수용 인원" />
+              최대 {space.capacity}인
+            </StyledCapacityInfo>
+          </StyledRightContent>
         </StyledSpaceInfo>
       </StyledSpaceContent>
     </StyledCard>
