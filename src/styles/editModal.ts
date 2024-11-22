b7a0375742cb975b1c94064ee0ed27c924807567
solmee <87332713+ssolfa@users.noreply.github.com>
2024-11-22 09:41:57 +0900

    fix: 1-3, 1-4 디자인 반영 (#11)
    * asset: 텍스트 로고 추가
    
    * feat: 전역 모달 스타일 추가
    
    * feat: 특정 경로에 푸터 제거
    
    * fix: 단체 정보 수정 페이지 디자인 반영
    
    * fix: 단체 정보 수정 페이지 디자인 반영
    
    * fix: 단체 정보 수정 페이지 이미지 배열로 변경
    
    * fix: 잘못 표기한 단체를 공간으로 수정

---
+++ src/styles/editModal.ts
@@ -0,0 +1,21 @@
+export const modalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: 0,
+    width: "630px",
+    height: "860px",
+    border: "none",
+    borderRadius: "20px",
+    overflow: "auto",
+    backgroundColor: "#FFFFFF",
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    zIndex: 1000,
+  },
+};
