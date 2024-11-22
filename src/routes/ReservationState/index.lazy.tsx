bfb7d8dfd3ba44c24497ae6f1610344a551a0c73
solmee <87332713+ssolfa@users.noreply.github.com>
2024-11-22 13:04:45 +0900

    feat: 2-5 공간 예약 현황 페이지 퍼블리싱 (#13)
    * setting: dayjs 설치
    
    * mock: mock 데이터 추가
    
    * feat: ReservationStateCard 컴포넌트 구현
    
    * feat: 공간 예약 현황 조회 페이지 구현
    
    * feat: 카드 클릭시 이벤트 추가 및 비밀번호 모달 추가

---
+++ src/routes/ReservationState/index.lazy.tsx
@@ -0,0 +1,33 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { ReservationStateCard } from "../-components/ReservationStateCard/ReservationStateCard";
+import { mockDateReservations } from "../../mock/reservationData";
+import { StyledContainer, StyledGrid, StyledTitle } from "./-index.style";
+
+export const Route = createLazyFileRoute("/ReservationState/")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const [selectedCardId, setSelectedCardId] = useState<number | null>(null);
+
+  const handleCardSelect = (id: number) => {
+    setSelectedCardId(selectedCardId === id ? null : id);
+  };
+
+  return (
+    <StyledContainer>
+      <StyledTitle>공간 예약 현황 조회</StyledTitle>
+      <StyledGrid>
+        {mockDateReservations.map((reservation) => (
+          <ReservationStateCard
+            key={reservation.id}
+            {...reservation}
+            isSelected={selectedCardId === reservation.id}
+            onCardSelect={() => handleCardSelect(reservation.id)}
+          />
+        ))}
+      </StyledGrid>
+    </StyledContainer>
+  );
+}
