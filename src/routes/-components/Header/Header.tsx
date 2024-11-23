2e176e50de7918b128724dce92176257c6ccba00
solmee <kii5541@naver.com>
2024-11-23 19:38:52 +0900

    feat: 로그인시만 1-1로 이동하도록 link 설정

--- src/routes/-components/Header/Header.tsx
+++ src/routes/-components/Header/Header.tsx
@@ -1,53 +1,61 @@
-import {
-    IconContainer,
-    SearchBar,
-    SearchContainer,
-    StyledHeader,
-    StyledLink, StyledLoginButton, StyledLogo,
-    StyledNav,
-} from "./Header.style.ts";
-import {useLoginState} from "../../../hooks/useLoginState.ts";
-import SignIn from "../SignIn/SignIn.tsx";
-import {useState} from "react";
+import { useState } from "react";
 import SearchIcon from "../../../assets/search.svg?react";
-import {api} from "../../../service/TokenService.ts";
+import { useLoginState } from "../../../hooks/useLoginState.ts";
+import { api } from "../../../service/TokenService.ts";
+import SignIn from "../SignIn/SignIn.tsx";
+import {
+  IconContainer,
+  SearchBar,
+  SearchContainer,
+  StyledHeader,
+  StyledLink,
+  StyledLoginButton,
+  StyledLogo,
+  StyledNav,
+} from "./Header.style.ts";
 
 function Header() {
-    const {isLoggedIn} = useLoginState();
-    const [signInModalOpen, setSignInModalOpen] = useState(false);
-    const closeModal = () => setSignInModalOpen(false);
+  const { isLoggedIn } = useLoginState();
+  const [signInModalOpen, setSignInModalOpen] = useState(false);
+  const closeModal = () => setSignInModalOpen(false);
+  const userId = api.getUserId();
 
-    return <StyledHeader>
-        <StyledLogo to='/'>OPENSSUpot</StyledLogo>
-        <StyledNav>
-            <StyledLink to="/">
-                공간오픈/관리
-            </StyledLink>
-            <StyledLink>
-                교내회의실 찾기
-            </StyledLink>
-            <SearchContainer>
-                <SearchBar placeholder="검색어를 입력하세요" />
-                <IconContainer>
-                    <SearchIcon style={{ minWidth: '25px' }} />
-                </IconContainer>
-            </SearchContainer>
-        </StyledNav>
-        <div>
-            {isLoggedIn ? (
-                <StyledLoginButton onClick={() => {api.logout(); window.location.reload();}}>
-                    로그아웃
-                </StyledLoginButton>
-            ) : (
-                <>
-                    <StyledLoginButton onClick={() => setSignInModalOpen(true)}>
-                        로그인
-                    </StyledLoginButton>
-                    <SignIn open={signInModalOpen} closeModal={closeModal} />
-                </>
-            )}
-        </div>
+  return (
+    <StyledHeader>
+      <StyledLogo to="/">OPENSSUpot</StyledLogo>
+      <StyledNav>
+        <StyledLink to={isLoggedIn ? `/organizations/${userId}` : "#"}>
+          공간오픈/관리
+        </StyledLink>
+        <StyledLink>교내회의실 찾기</StyledLink>
+        <SearchContainer>
+          <SearchBar placeholder="검색어를 입력하세요" />
+          <IconContainer>
+            <SearchIcon style={{ minWidth: "25px" }} />
+          </IconContainer>
+        </SearchContainer>
+      </StyledNav>
+      <div>
+        {isLoggedIn ? (
+          <StyledLoginButton
+            onClick={() => {
+              api.logout();
+              window.location.reload();
+            }}
+          >
+            로그아웃
+          </StyledLoginButton>
+        ) : (
+          <>
+            <StyledLoginButton onClick={() => setSignInModalOpen(true)}>
+              로그인
+            </StyledLoginButton>
+            <SignIn open={signInModalOpen} closeModal={closeModal} />
+          </>
+        )}
+      </div>
     </StyledHeader>
+  );
 }
 
 export default Header;
