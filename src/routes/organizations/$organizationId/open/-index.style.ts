642f6b48f2798fc87c7c1e01637f3ee5ee13b105
solmee <kii5541@naver.com>
2024-11-24 21:09:58 +0900

    fix: 사진 크기 수정

--- src/routes/organizations/$organizationId/open/-index.style.ts
+++ src/routes/organizations/$organizationId/open/-index.style.ts
@@ -84,9 +84,9 @@ export const StyledImageUploadBox = styled.div<{
   $isEmpty?: boolean;
   $isMain?: boolean;
 }>`
-  width: ${(props) => (props.$isEmpty ? "100%" : "260px")};
-  aspect-ratio: ${(props) => (props.$isEmpty ? "auto" : "16/10")};
-  min-height: ${(props) => (props.$isEmpty ? "200px" : "auto")};
+  width: ${(props) => (props.$isEmpty ? "560px" : "560px")};
+  /* aspect-ratio: ${(props) => (props.$isEmpty ? "auto" : "16/10")}; */
+  min-height: ${(props) => (props.$isEmpty ? "420px" : "420px")};
   border: 1px solid #e5e8eb;
   border-radius: 5px;
   display: flex;
