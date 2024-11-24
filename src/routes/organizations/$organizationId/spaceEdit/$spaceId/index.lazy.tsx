09cf8fd601c6fb5056c501925505ab497ec57859
solmee <kii5541@naver.com>
2024-11-24 21:04:54 +0900

    fix: timepicker 수정

--- src/routes/organizations/$organizationId/spaceEdit/$spaceId/index.lazy.tsx
+++ src/routes/organizations/$organizationId/spaceEdit/$spaceId/index.lazy.tsx
@@ -154,12 +154,18 @@ function RouteComponent() {
           <StyledTimeContainer>
             <StyledTimeInput
               type="time"
+              step="1800"
+              min="06:00"
+              max="21:00"
               {...register("openingTime", { required: true })}
               placeholder="오픈 시간을 선택하세요"
             />
             <span>~</span>
             <StyledTimeInput
               type="time"
+              step="1800"
+              min="06:00"
+              max="21:00"
               {...register("closingTime", { required: true })}
               placeholder="종료 시간을 선택하세요"
             />
