c7205a748d12d7fdaf380755de4da3d1d5ca07f0
owl1753 <bighammer1753@gmail.com>
2024-11-24 19:23:07 +0900

    chore: 캐싱 제거

--- src/hooks/useGetMeetingRooms.ts
+++ src/hooks/useGetMeetingRooms.ts
@@ -3,6 +3,8 @@ import {getMeetingRooms, GetMeetingRoomsParams} from "../api/getMeetingRooms.ts"
 
 export const useGetMeetingRooms = ({startDateTime, endDateTime}: GetMeetingRoomsParams) => {
     return useQuery({
+        gcTime: 0,
+        staleTime: 0,
         queryKey: ['meetingRooms', startDateTime, endDateTime],
         queryFn: () => getMeetingRooms({startDateTime, endDateTime}),
     })
